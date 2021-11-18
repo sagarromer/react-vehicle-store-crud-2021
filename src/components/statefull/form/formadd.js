f8ca02c0d4de9a5e57b99e23f0796dbb9453dca2
sagara dissanayake <39671491+sagarromer@users.noreply.github.com>
2021-11-18 04:24:31 -0800

    form component implemented

--- src/components/statefull/form/formadd.js
+++ src/components/statefull/form/formadd.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react'
-
+import Formtitle from "../../../components/stateless/formtitle/formtitle"
 class formadd extends Component {
     render() {
         return (
             <>
-                <h1>Add car</h1>
+                <Formtitle>Add car</Formtitle>
+                <form>
+                    <div className="form-group">
+                        <label>brand</label>
+                        <input className="form-control" type="text" id="brand"></input>
+                    </div>
+                    <div className="form-group">
+                        <label>color</label>
+                        <input className="form-control" type="text" id="color"></input>
+                    </div>
+                    <div className="form-group">
+                        <label>price</label>
+                        <input className="form-control" type="number" id="price"></input>
+                    </div>
+                    <div className="form-group">
+                        <button className="btn btn-primary">validate</button>
+                                            </div>
+                </form>
             </>
         )
     }
---
+++ src/components/stateless/formtitle/formtitle.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "./formtitle.module.css"
+
+function formtitle() {
+    return (
+        <h1 className={`${styles.fontHeading} text-center`}>
+            add vehicle
+        </h1>
+    )
+}
+
+export default formtitle
