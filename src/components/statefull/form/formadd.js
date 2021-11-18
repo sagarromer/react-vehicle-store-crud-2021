76191697fd57477579d1a8979dd5341828cf2b1f
sagara dissanayake <39671491+sagarromer@users.noreply.github.com>
2021-11-18 05:16:41 -0800

    datat add to form state completed

--- src/components/statefull/form/formadd.js
+++ src/components/statefull/form/formadd.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react'
 import Formtitle from "../../../components/stateless/formtitle/formtitle"
+import Button from "../../../components/stateless/button/Button"
 class formadd extends Component {
+
+    state = {
+        brand : "", 
+        color: "",
+        price: ""
+    }
+    validationHandler = (event) => {
+        event.preventDefault();
+        console.log(this.state);
+        
+    }
     render() {
         return (
             <>
@@ -8,18 +20,27 @@ class formadd extends Component {
                 <form>
                     <div className="form-group">
                         <label>brand</label>
-                        <input className="form-control" type="text" id="brand"></input>
+                        <input className="form-control" type="text" 
+                        id="brand" onChange={(event) => this.setState({brand : event.target.value})}
+                        value={this.state.brand}/>
                     </div>
                     <div className="form-group">
                         <label>color</label>
-                        <input className="form-control" type="text" id="color"></input>
+                        <input className="form-control" 
+                        type="text" id="color" 
+                        onChange={(event) => this.setState({color : event.target.value})}
+                        value={this.state.color}/>
+                        
                     </div>
                     <div className="form-group">
                         <label>price</label>
-                        <input className="form-control" type="number" id="price"></input>
+                        <input className="form-control" type="number" id="price" 
+                        onChange={(event) => this.setState({price : event.target.value})}
+                        value={this.state.price}/>
+                        
                     </div>
                     <div className="form-group">
-                        <button className="btn btn-primary">validate</button>
+                        <Button clic = {this.validationHandler} className="btn btn-primary">validate</Button>
                                             </div>
                 </form>
             </>
