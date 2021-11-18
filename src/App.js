ee1bd2b229830e9c429e7eb093e18b1b2e96fcbe
sagara dissanayake <39671491+sagarromer@users.noreply.github.com>
2021-11-18 02:55:10 -0800

    add button toggle implemented

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import React, { Component } from 'react'
 import Heading from "./components/stateless/heading/Heading"
 import Cars from "./components/statefull/cars/Cars"
 import Button from "./components/stateless/button/Button"
+import Formadd from "./components/statefull/form/formadd"
 class App extends Component {
+
+  state = {
+    isFormOpen : false
+  }
   render() {
     return (
       <div className="container">
 
         <Heading>vehicle center</Heading>
-        <Cars></Cars>        
-        <Button typebtn="btn-success w-100">Add</Button>
+        <Cars></Cars>  
+        {
+          this.state.isFormOpen && <Formadd />
+        }      
+        <Button clic = {
+          () => this.setState((prevState) => ({isFormOpen : !prevState.isFormOpen}))
+        } typebtn="btn-success w-100">
+          {this.state.isFormOpen ? "close" : "Add"}
+        </Button>
       </div>
     );
   }
---
+++ src/components/statefull/form/formadd.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class formadd extends Component {
+    render() {
+        return (
+            <>
+                <h1>Add car</h1>
+            </>
+        )
+    }
+}
+
+export default formadd
