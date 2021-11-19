155058cf1db8afeac65178c9612f3ea5816ddbea
sagara dissanayake <39671491+sagarromer@users.noreply.github.com>
2021-11-18 23:09:05 -0800

    local storage implemented for data persistence

--- src/components/statefull/cars/Cars.js
+++ src/components/statefull/cars/Cars.js
@@ -6,12 +6,8 @@ import Fade from 'react-reveal/Fade'
 class Cars extends Component {
 
     state ={
-        myCars : [
-            {id : 1, brand : "Toyota", color: "black", price: 50000},
-            {id : 2, brand : "Mercedez", color: "green", price: 30000},
-            {id : 3, brand : "Mazda", color: "red", price: 20000},
-        ],
-        lastid : 3,
+        myCars : localStorage.getItem("newcars") ? JSON.parse(localStorage.getItem("newcars")) : [],
+        lastid : localStorage.getItem("lastid") ? JSON.parse(localStorage.getItem("lastid")) : 0, 
         isDataEditing : 0
     }
     updateCarHandler = (id, brand, color, price) => {
@@ -47,11 +43,31 @@ class Cars extends Component {
             myCars :newCars,
             isDataEditing : 0
         })
+        localStorage.setItem("newCars", JSON.stringify(newCars));
+
         
     }
     editCarHandler = () => {
         console.log('you have clicked on edit button');
         
+    }
+    addCarHandler = (brand, color, price) => {
+        console.log('you have clicked on edit button');
+        const newCar = {
+            id : this.state.lastId + 1, 
+            brand: brand, 
+            color: color,
+            price : price
+        }
+        const lastId = this.state.lastid + 1;
+        const newCars = [...this.state.myCars];
+        newCars.push(newCar);
+        this.setState((prevState) => ({
+            myCars : newCars,
+            lastId : prevState.lastid +1
+        }))
+        localStorage.setItem("newCars", JSON.stringify(newCars));
+        localStorage.setItem("lastid", JSON.stringify(lastId));
     }
     render() {
         return (
